d7ad181817b19e54453bcde147408a71c2c7e57f
Sean Chen <sean.chen@leocorn.com>
2018-07-31 17:37:43 +0000

    allow local.js to overwrite config.js

--- src/config.js
+++ src/config.js
@@ -1,12 +1,25 @@
 /**
  * configuration for the application
  */
+const fs = require('fs');
 
 var config = {
-
+    /**
+     * here are some default values.
+     */
     spoUrl: "https://sites.sharepoint.com",
     username: 'someone@outlook.com',
-    passowrd: 'somepassword'
+    password: 'somepassword'
+
+};
+
+// check if the local.js exist?
+if(fs.existsSync('./local.js')) {
+    // file not exist. 
+    console.log("Could not find local.js! Consider create one!");
+} else {
+    const local = require('./local');
+    config = Object.assign(config, local);
 }
 
 module.exports = config;
