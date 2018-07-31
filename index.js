a08384e08a34f88d28f0965c76a4eedd6426b7e8
Sean Chen <sean.chen@leocorn.com>
2018-07-31 16:46:07 +0000

    try the configuration file.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // we will set the export default statements here.
 var hello = require('./src/hello');
+var config = require('./src/config');
 
 hello.say("Hello require!");
+hello.say("Config: " + JSON.stringify(config, null, 2));
---
+++ src/config.js
@@ -0,0 +1,12 @@
+/**
+ * configuration for the application
+ */
+
+var config = {
+
+    spoUrl: "https://sites.sharepoint.com",
+    username: 'someone@outlook.com',
+    passowrd: 'somepassword'
+}
+
+module.exports = config;
