cefd74890548443a3b9d4345e740ae3c64711dcc
Sean Chen <sean.chen@leocorn.com>
2018-08-01 13:56:29 +0000

    keep this file as simple as posible.

--- index.js
+++ index.js
@@ -1,33 +1,6 @@
-// we will set the export default statements here.
+/**
+ * keep this file as simple as possible.
+ */
+
 const config = require('./src/config');
-const spo = require('node-sp-auth');
-const request = require('request');
-
 console.log(JSON.stringify(config, null, 2));
-
-spo.getAuth(config.spoUrl, {username: config.username, 
-                            password: config.password})
-.then(options => {
-    // let's check the options.
-    // it only contains a cookie which will have the
-    // access token.
-    console.dir(options);
-
-    // perform request with any http-enabled library 
-    // (request-promise in a sample below):
-    let headers = options.headers;
-    //headers['Accept'] = 'application/json;odata=verbose';
-    headers['Accept'] = 'application/json';
-    filePath = config.samplePathes[0];
-    let theUrl = config.spoUrl + config.spoSite + filePath;
-    console.log(theUrl);
-
-    request.get({
-        requestContentType: 'JSON',
-        url: `${theUrl}`,
-        headers: headers
-    }, function(error, response, body) {
-        //process data
-        console.dir(response);
-    });
-});
