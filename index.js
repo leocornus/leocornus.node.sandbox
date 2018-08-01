233841e9846cc61ba014848dbf5d08b4dc01ee0a
Sean Chen <sean.chen@leocorn.com>
2018-08-01 12:12:02 +0000

    using the node-sp-auth to login.

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 // we will set the export default statements here.
 const config = require('./src/config');
+const spo = require('node-sp-auth');
 
 console.log(JSON.stringify(config, null, 2));
+
+spo.getAuth(config.spoUrl, {username: config.username, 
+                            password: config.password})
+.then(options => {
+    // let's check the options.
+    // it only contains a cookie which will have the
+    // access token.
+    console.dir(options);
+});
