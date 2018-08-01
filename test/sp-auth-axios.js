4bb8a5d6cbc1900631b8882044a0e82299be08c5
Sean Chen <sean.chen@leocorn.com>
2018-08-01 17:28:27 +0000

    some format change

--- test/sp-auth-axios.js
+++ test/sp-auth-axios.js
@@ -9,21 +9,20 @@
 // we have to use the ./ as current foler.
 const config = require('./../src/config');
 // the 
-const spo = require('node-sp-auth');
+const spoAuth = require('node-sp-auth');
 const axios = require('axios');
 
 console.log(JSON.stringify(config, null, 2));
 
-spo.getAuth(config.spoUrl, {username: config.username, 
-                            password: config.password})
+spoAuth.getAuth(config.spoUrl, 
+            {username: config.username, password: config.password})
 .then(options => {
+
     // let's check the options.
     // it only contains a cookie which will have the
     // access token.
-    console.dir(options);
+    //console.dir(options);
 
-    // perform request with any http-enabled library 
-    // (request-promise in a sample below):
     // get ready header.
     let headers = options.headers;
     //headers['Accept'] = 'application/json;odata=verbose';
@@ -39,9 +38,10 @@ spo.getAuth(config.spoUrl, {username: config.username,
       method: "get",
       headers: headers,
     };
-
+    // call the API to get response.
     axios.request(reqConfig).then(function(response) {
         // process data
         console.dir(response);
     });
 });
+
