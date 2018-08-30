1b125e84af9d5d8571711260d46436a2580b2f53
Sean Chen <sean.chen@leocorn.com>
2018-08-30 14:11:49 +0000

    The correct way to doe async work.

--- test/async-iterate.js
+++ test/async-iterate.js
@@ -15,26 +15,70 @@ function PrintTotalSum() {
 
 /**
  * =================================================================
- * Solution Two - Asynchronus read 
+ * Solution Three - Correct Asynchronous read
+ * 
+ */
+function IterateOver(list, iterator, callback) {
+
+    // this is the function that will start all the jobs
+    // list is the collections of item we want to iterate over
+    // iterator is a function representing the job when want done on each item
+    // callback is the function we want to call when all iterations are over
+
+    var doneCount = 0;  // here we'll keep track of how many reports we've got
+
+    function report() {
+        // this function resembles the phone number in the analogy above
+        // given to each call of the iterator so it can report its completion
+
+        doneCount++;
+
+        // if doneCount equals the number of items in list, then we're done
+        if(doneCount === list.length)
+            callback();
+    }
+
+    // here we give each iteration its job
+    for(var i = 0; i < list.length; i++) {
+        // iterator takes 2 arguments, an item to work on and report function
+        iterator(list[i], report)
+    }
+}
+
+IterateOver(paths, function(path, report) {
+
+    fs.readFile(path, 'utf8', function(err, data) {
+
+        var num = parseInt(data);
+        totalSum += num;
+
+        // we must call report to report back iteration completion
+        report();
+    });
+}, PrintTotalSum);
+
+/**
+ * =================================================================
+ * Solution Two - Asynchronous read 
  * -- Fast enough! But not working!
  *
  * The asynchronous way to read each file. 
  * Here the hehavour will become tricky!
  */
-for(var i = 0; i < paths.length; i++) {
-
-    console.log("reading file: " + paths[i]);
-    // readFile is asynchronous action.
-    fs.readFile(paths[i], 'utf8', function(err, data) {
-        var num = parseInt(data);
-        totalSum += num;
-    });
-    console.log("DONE reading file: " + paths[i]);
-}
-
-// invoke the callback
-// NOTE: the total sum will always be 0!
-PrintTotalSum();
+//for(var i = 0; i < paths.length; i++) {
+//
+//    console.log("reading file: " + paths[i]);
+//    // readFile is asynchronous action.
+//    fs.readFile(paths[i], 'utf8', function(err, data) {
+//        var num = parseInt(data);
+//        totalSum += num;
+//    });
+//    console.log("DONE reading file: " + paths[i]);
+//}
+//
+//// invoke the callback
+//// NOTE: the total sum will always be 0!
+//PrintTotalSum();
 
 /**
  * =================================================================
