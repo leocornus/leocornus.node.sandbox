c8f055aa238bedb1745a3e216b69383bd076f4e4
Sean Chen <sean.chen@leocorn.com>
2018-08-30 13:05:13 +0000

    add the asynchronous solution to read file, it is not working!

--- test/async-iterate.js
+++ test/async-iterate.js
@@ -13,17 +13,42 @@ function PrintTotalSum() {
     console.log(totalSum); 
 }
 
-/*
+/**
+ * =================================================================
+ * Solution Two - Asynchronus read 
+ * -- Fast enough! But not working!
+ *
+ * The asynchronous way to read each file. 
+ * Here the hehavour will become tricky!
+ */
+for(var i = 0; i < paths.length; i++) {
+
+    console.log("reading file: " + paths[i]);
+    // readFile is asynchronous action.
+    fs.readFile(paths[i], 'utf8', function(err, data) {
+        var num = parseInt(data);
+        totalSum += num;
+    });
+    console.log("DONE reading file: " + paths[i]);
+}
+
+// invoke the callback
+// NOTE: the total sum will always be 0!
+PrintTotalSum();
+
+/**
  * =================================================================
  * Solution One - Synchronous read.
+ * -- it is working by not efficient!
  *
  * the synchronous way to read file.
  * The for loop will read file one by one!
  */
-for(var i = 0; i < paths.length; i++) {
-    var num = parseInt(fs.readFileSync(paths[i], 'utf8'));
-    totalSum += num;
-}
+//for(var i = 0; i < paths.length; i++) {
+//    var num = parseInt(fs.readFileSync(paths[i], 'utf8'));
+//    totalSum += num;
+//}
+//
+//// invoke the callback
+//PrintTotalSum();
 
-// invoke the callback
-PrintTotalSum();
