9ca83f55be9b54c2a9294626a116148e88919b2f
Sean Chen <sean.chen@leocorn.com>
2018-08-30 12:30:57 +0000

    try to control the threads, it is NOT working...

--- src/spo.js
+++ src/spo.js
@@ -11,6 +11,10 @@ var spo = {
     pathes: [],
     // error count.
     errorCount: 0,
+    // set teh cap for threads, how many concurrent threads at one time.
+    threadCap: 1000,
+    // the count start from 1,
+    threadCount: 1,
 
     /**
      * try to interate into a folder.
@@ -26,7 +30,7 @@ var spo = {
                    "/_api/web/GetFolderByServerRelativeUrl('" +
                    //encodeURIComponent(folderName) + "')";
                    folderName + "')";
-      console.log(`${vm.folderCount} : ${folderName},${theUrl}`);
+      console.log(`${vm.folderCount}[${vm.threadCount}] : ${folderName},${theUrl}`);
       var reqConfig = {
           headers: headers
       };
@@ -36,14 +40,43 @@ var spo = {
       // process sub folders.
       axios.get(theUrl + "/Folders", reqConfig).then(function(response) {
           //console.log(response);
+
+          // got response, we will consider the request finished.
+          vm.threadCount --;
+
           response.data.value.forEach(function(folder, index) {
               //console.log(folder);
               var subFolderName = folder.Name;
-              // set timeout for each subfolder.
-              setTimeout(function() {
-                  vm.processFolder(siteUrl, folderName + "/" + subFolderName,
-                                   headers);
-              }, 50 * index);
+
+              // check if we reach the thread cap.
+              if(vm.threadCount >= vm.threadCap) {
+                  //var checkCap = function() {
+                  //    if(vm.threadCount >= vm.threadCap) {
+                  //        setTimeout(function() {
+                  //            console.log("waiting...");
+                  //            return false;
+                  //        }, 50);
+                  //    } else {
+                  //        return true;
+                  //    }
+                  //};
+                  //while(!checkCap()) {
+                  //    // do nothing, keep checking...
+                  //}
+                  // set timeout for each subfolder.
+                  setTimeout(function() {
+                      //vm.processFolder(siteUrl, 
+                      //                 folderName + "/" + subFolderName,
+                      //                 headers);
+                      //vm.threadCount ++;
+                      console.log("-------------- wait done");
+                  }, 100);
+              }
+
+              vm.processFolder(siteUrl, 
+                               folderName + "/" + subFolderName,
+                               headers);
+              vm.threadCount ++;
           });
       }).catch(function(error) {
           vm.errorCount ++;
