150885732879d63bd4154a301c3f9f80a647af29
Sean Chen <sean.chen@leocorn.com>
2018-09-07 13:23:31 +0000

    create the matchOver function to asynchornously iterate over all patterns. we will have many patterns.

--- test/sync-solr-extract.js
+++ test/sync-solr-extract.js
@@ -49,8 +49,21 @@ axios.get(solrEndpoint, totalQuery)
                 doc["version_extract"] = 0.1;
                 // TODO extract the model from file content.
                 if(doc.hasOwnProperty('file_content')) {
-                    doc["product_models"] =
-                        extractProductModels(doc["file_content"]);
+                    // the quick test code.
+                    //doc["product_models"] =
+                    //    extractProductModels(doc["file_content"]);
+                    matchOver(doc["file_content"],
+                              config.modelPatterns,
+                              function(content, onePattern, reportMatches) {
+                                  // try to find matches:
+                                  let matches = 
+                                      Array.from(new Set(content.match(onePattern)));
+                                  reportMatches(matches);
+                              },
+                              function(allMatches) {
+                                  // set to 
+                                  doc["product_models"] = [].concat(allMatches);
+                              });
                 }
                 doc["_version_"] = 0;
                 return doc;
@@ -165,7 +178,8 @@ function extractProductModels(fileContent) {
 
     // analyse the file content.
     //console.log(fileContent);
-    let patterns = [/[A-Z]+-[0-9]+/g,
+    let patterns = [
+        /[A-Z]+-[0-9]+/g,
         // using " " for whitespace, \s will take line break as 
         // white space too.
         /[A-Z]{3} [0-9]{3} [0-9]{2} {0,1}[A-Z]{2}/g, // LTL 040 40 EF
@@ -178,4 +192,36 @@ function extractProductModels(fileContent) {
     console.log(matches_1);
 
     return matches.concat(matches_1);
+}
+
+/**
+ * match over through all patterns.
+ *
+ * we will try the async approach first.
+ */
+function matchOver(fileContent, patterns, extractor, callback) {
+    // set the count
+    var doneCount = 0;
+    var matches = [];
+
+    // get ready the reportMatches function.
+    function reportMatches(theMatches) {
+        // increase the counting...
+        doneCount ++;
+
+        // keep the matches.
+        matches.push(theMatches);
+        console.log("find " + theMatches.length + " Matches");
+
+        // check if it donw.
+        if(doneCount === patterns.length) {
+            // pass the full matches to callback.
+            callback(matches);
+        }
+    }
+
+    for(var i = 0; i < patterns.length; i++) {
+        // go through each pattern.
+        extractor(fileContent, patterns[i], reportMatches);
+    }
 }
