64ff50dbc0bdd4b152d8c40567eb6ea47ef6356d
Sean Chen <sean.chen@leocorn.com>
2018-11-02 14:59:08 +0000

    give good name for the iterator and add more comments for reading

--- test/strategy.js
+++ test/strategy.js
@@ -3,20 +3,34 @@
  */
 
 const strategy = require('./../src/strategy');
+const prettyMs = require('pretty-ms');
 
 const now = () => new Date().toUTCString()
 
-var dummyIterator = function(start, reportDone) {
+/**
+ * Half second (500 ms) ticker iterator.
+ * incremental counting by 1 for each half second.
+ */
+var halfSecond = function(start, reportDone) {
 
-        // just print out timestamp.
+    // just print out timestamp.
     console.log(now() + " - working on " + start);
+
     setTimeout(function() {
         reportDone(1);
     }, 500);
 };
 
-strategy.waterfallOver(10000, dummyIterator, 
+// set the start time.
+var startTime = new Date();
+
+// count 100 times and each time take half second.
+// we should complete in 50 seconds.
+strategy.waterfallOver(100, halfSecond,
+
     function() {
-        console.log(now() + " All Done");
+        // the complete callback funtion.
+        var totalTime = (new Date()) - startTime;
+        console.log(now() + " All Done in " + prettyMs(totalTime));
     }
 );
