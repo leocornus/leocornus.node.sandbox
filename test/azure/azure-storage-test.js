e5b561fcffd359c11c9184567ecc65de63f8b796
Sean Chen <sean.chen@leocorn.com>
2018-11-26 19:53:31 +0000

    quick test to download file from Azure storage file share

--- test/azure/azure-storage-test.js
+++ test/azure/azure-storage-test.js
@@ -1,4 +1,6 @@
 const config = require('./../../src/config');
+const fs = require('fs');
+
 const azure = require('azure-storage');
 
 var fileService = azure.createFileService(config.azure.storageAccount, 
@@ -21,6 +23,18 @@ fileService.createDirectoryIfNotExists(config.azure.storageFileShare, 'csa',
         // if result = true, share was created.
         // if result = false, share already existed.
         console.log("Directory ----------------");
+        console.dir(response);
+    }
+});
+
+// get file to stream.
+fileService.getFileToStream(config.azure.storageFileShare, 'csa', '2410984.pdf',
+                            fs.createWriteStream('output.pdf'),
+                            function(error, result, response) {
+    if (!error) {
+        // if result = true, share was created.
+        // if result = false, share already existed.
+        console.log("getFile----------------");
         console.dir(response);
     }
 });
