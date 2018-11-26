c7f84f0e87f0ec3ea9e8d6a980d2194b01dafb61
Sean Chen <sean.chen@leocorn.com>
2018-11-26 19:45:50 +0000

    quick test for accessing Azure storage file share directory

--- test/azure/azure-storage-test.js
+++ test/azure/azure-storage-test.js
@@ -4,11 +4,23 @@ const azure = require('azure-storage');
 var fileService = azure.createFileService(config.azure.storageAccount, 
                                           config.azure.storageAccessKey);
 
+// Access Share!
 fileService.createShareIfNotExists(config.azure.storageFileShare, 
                                    function(error, result, response) {
     if (!error) {
         // if result = true, share was created.
         // if result = false, share already existed.
+        console.dir(response);
+    }
+});
+
+// access directory.
+fileService.createDirectoryIfNotExists(config.azure.storageFileShare, 'csa',
+                                       function(error, result, response) {
+    if (!error) {
+        // if result = true, share was created.
+        // if result = false, share already existed.
+        console.log("Directory ----------------");
         console.dir(response);
     }
 });
