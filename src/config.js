6199f3a8e61f453debc0456b5e2377c95aea4ed1
Sean Chen <sean.chen@leocorn.com>
2018-11-28 14:39:59 +0000

    add comments about Object assign method.

--- src/config.js
+++ src/config.js
@@ -26,6 +26,10 @@ if(fs.existsSync('./local.js')) {
     console.log("Could not find local.js! Consider create one!");
 } else {
     const local = require('./local');
+    // the first arg is the target Object,
+    // all other objects are source objects.
+    // The properties in source Object will override
+    // the properties in target object.
     config = Object.assign(config, local);
 }
 
