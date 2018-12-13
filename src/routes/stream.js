562694284cead5cc4522cd7a704925dc3ff29c34
Sean Chen <sean.chen@leocorn.com>
2018-12-13 18:05:20 +0000

    quick test for request query parameters

--- src/routes/stream.js
+++ src/routes/stream.js
@@ -1,14 +1,34 @@
 "use strict";
 
 let config = require("../config");
+let fs = require('fs');
 
 let azure = require('azure-storage');
+let fileService = azure.createFileService(config.azure.storageAccount, 
+                                          config.azure.storageAccessKey);
 
 module.exports = function(app) {
 
-    // Login page
+    // stream file. 
     app.get("/stream/file", function(req, res) {
 
-        res.send(`File: ${req.query.path}`);
+        // check the path exist or not.
+        if(req.query.hasOwnProperty('path')) {
+
+            //res.send(`File: ${req.query.path}`);
+            // get file to stream.
+            fileService.getFileToStream(config.azure.storageFileShare, 'csa', '2410984.pdf',
+                                        fs.createWriteStream('/tmp/output.pdf'),
+                                        function(error, result, response) {
+                if (!error) {
+                    // if result = true, share was created.
+                    // if result = false, share already existed.
+                    console.log("getFile----------------");
+                    res.send(response);
+                }
+            });
+        } else {
+            res.send('Please specify file path!');
+        }
     });
 };
