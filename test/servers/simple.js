33d1bc68cbb941e16d13ce8cd9f5ae08c586a354
Ubuntu <ubuntu@ip-172-31-5-126.us-west-2.compute.internal>
2019-01-02 18:20:59 +0000

    add some simple dummy express server for testing

---
+++ test/servers/simple.js
@@ -0,0 +1,23 @@
+// the very simple express server.
+
+var express = require('express');
+var app = express();
+
+// hello world simple get.
+app.get('/', function(req, res) {
+    res.send('<h1>Hello Leocornus LTD!</h1>');
+});
+
+// explore the request and response...
+// simply echo the request to response.
+app.get('/echo', function(req, res) {
+
+    var query = req.query;
+    // the request query is an object.
+    //console.log(query);
+    res.send(query);
+});
+
+// start server.
+var server = app.listen(19888, function() {
+});
