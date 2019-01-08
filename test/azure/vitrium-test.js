1019447754762a51f8506687dfc080363a203a15
Sean Chen <sean.chen@leocorn.com>
2019-01-08 14:59:39 +0000

    get started a new test case for vitrium

---
+++ test/azure/vitrium-test.js
@@ -0,0 +1,16 @@
+/**
+ * simple test case to show how to use Vitrium APIs.
+ *
+ * How to run the test.
+ *
+ *  $ cd src
+ *  $ ln -s ....vitrium.js local.js // link the local.js for vitrium
+ *  $ cd ..
+ *  $ nvm run node test/azure/vitrium-test.js
+ */
+
+const axios = require('axios');
+// we should have separate local.js file for vitrium.
+const config = require('./../../src/config');
+
+console.log(config);
