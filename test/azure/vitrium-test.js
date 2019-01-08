6ab9f3a6f4a98823c002d7c876216258ce994aec
Sean Chen <sean.chen@leocorn.com>
2019-01-08 15:49:40 +0000

    the first step, challenge server to get server nonce

--- test/azure/vitrium-test.js
+++ test/azure/vitrium-test.js
@@ -10,7 +10,47 @@
  */
 
 const axios = require('axios');
+// using the UUID v4.
+const uuidv4 = require('uuid/v4');
 // we should have separate local.js file for vitrium.
 const config = require('./../../src/config');
 
-console.log(config);
+// set up the request header.
+let headers = [];
+headers['X-VITR-ACCOUNT-TOKEN'] = config.vitrium.accountToken;
+headers["X-VITR-SESSION-TOKEN"] = config.vitrium.sessionToken;
+
+// set the client nonce
+let clientNonce = uuidv4();
+
+// set the payload
+let payload = {
+  "ClientNonce": clientNonce
+}
+
+/**
+ * Step 1: get challenge server to get a ServerNonce.
+ */
+// get ready the axios request config
+let reqConf = {
+  url: config.vitrium.docApiBaseUrl + '/Login/Challenge',
+  method: 'post',
+  header: headers,
+  data: payload
+}
+
+// challenge to get ServerNonce.
+axios.request(reqConf).then(function(response) {
+
+    /**
+     * Step 2: get the login response.
+     */
+    console.dir(response.data);
+    let serverNonce = response.data.ServerNonce;
+
+    // get ready the client Hash:
+    //let clientHash = gen
+}).catch(function(error) {
+
+    console.dir(error);
+});
