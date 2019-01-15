3dcf7118eaaa2d68e79bfb39cc3cfe4301af0ae3
Sean Chen <sean.chen@leocorn.com>
2019-01-15 19:34:07 +0000

    add the test case to get multiple docs.

---
+++ test/azure/vitrium-doc-test.js
@@ -0,0 +1,24 @@
+/**
+ * simple test case to show how to use Vitrium APIs.
+ *
+ * How to run the test.
+ *
+ *  $ cd src
+ *  $ ln -s ....vitrium.js local.js // link the local.js for vitrium
+ *  $ cd ..
+ *  $ nvm run node test/azure/vitrium-policy-test.js
+ */
+
+// we should have separate local.js file for vitrium.
+const config = require('./../../src/config');
+
+const vitrium = require('./../../src/libs/vitrium');
+
+// quick test to get all policies.
+vitrium.getDocs(config.vitrium.oAccountToken,
+                config.vitrium.oSessionToken,
+                0, 5,
+                (res, err) => {
+
+    console.log(res.data.Results.length);
+});
