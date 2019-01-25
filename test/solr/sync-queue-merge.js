0e7c43a4d258938b47afe236c6d8e85d1aa9a685
Sean Chen <sean.chen@leocorn.com>
2019-01-25 02:19:23 +0000

    more logic for the queue process.

--- test/solr/sync-queue-merge.js
+++ test/solr/sync-queue-merge.js
@@ -76,88 +76,99 @@ axios.get(sourceSelect, totalQuery)
             // handle response here.
             //console.log("Got Response:");
             //console.dir(response.data.response.docs.length);
+
             // async call to iterate each doc.
             let payload = response.data.response.docs;
             strategy.iterateOver(payload,
             function(doc, report) {
-                // preparying the query.
-                let query = {
-                  params: {
-                    q: `${localConfig.idField}:${doc[localConfig.idField]}`
-                  }
-                };
-                // inspect query.
-                //console.log(query);
+                // brief steps:
+                // - update source to processing
+                delete doc["_version_"];
+                doc["process_status"] = "processing";
+                doc["process_message"] = "start processing";
+                axios.post(sourceUpdate, doc
+                ).then(function(suRes) {
+                    // source update response.
 
-                // query the matched doc.
-                axios.get(targetSelect, query)
-                .then(function(res) {
+                    // - copy metada to target
+                    // preparying the query to search target.
+                    let query = {
+                      params: {
+                        q: `${localConfig.idField}:${doc[localConfig.idField]}`
+                      }
+                    };
+                    // inspect query.
+                    //console.log(query);
 
-                    // merge docs to one doc.
-                    let theDoc = localConfig.mergeDoc(res.data.response.docs[0], 
-                                                      doc);
-                    //console.dir(theDoc);
-                    if(theDoc === null) {
-                        // target has the source content!
-                        // skip it!
-                        console.log(`File exist! Skip - ${doc[localConfig.idField]}`);
+                    // query the matched doc in target collection
+                    axios.get(targetSelect, query)
+                    .then(function(res) {
+
+                        // merge docs to one doc.
+                        let theDoc = localConfig.mergeDoc(res.data.response.docs[0],
+                                                          doc);
+                        //console.dir(theDoc);
+                        if(theDoc === null) {
+                            // target has the source content!
+                            // skip it!
+                            console.log(`File exist! Skip - ${doc[localConfig.idField]}`);
+                            report();
+                        }
+                        // post to target.
+                        axios.post(targetUpdate, theDoc
+                        ).then(function(postRes) {
+                            //console.log("Post Success!");
+                            report();
+                            //console.dir(postRes);
+                            doc['process_status'] = 'success-metadata';
+                            doc['process_message'] = 'Metadata process success';
+                            // update the source document.
+                            axios.post(sourceUpdate, doc
+                            ).then(function(su1Res) {
+
+                            }).catch(function(su1Err) {
+
+                            });
+                        }).catch(function(postError) {
+                            console.log("Post Failed! - " + theDoc[localConfig.idField]);
+                            //console.dir(postError);
+                            // log the erorr and then report the copy is done!
+                            report();
+                            doc['process_status'] = 'failed-metadata';
+                            doc['process_message'] = 'Metadata process Failed!';
+                            // update the source document.
+                            axios.post(sourceUpdate, doc
+                            ).then(function(su2Res) {
+
+                            }).catch(function(su2Err) {
+
+                            });
+                        });
+
+                    }).catch(function(err) {
+                        console.log("Query Failed! - " + doc[localConfig.idField]);
+                        //console.dir(err);
                         report();
-                    }
-                    // post to target.
-                    axios.post(targetUpdate, theDoc
-                    ).then(function(postRes) {
-                        //console.log("Post Success!");
-                        report();
-                        //console.dir(postRes);
-                    }).catch(function(postError) {
-                        console.log("Post Failed! - " + theDoc[localConfig.idField]);
-                        //console.dir(postError);
-                        // log the erorr and then report the copy is done!
-                        report();
+                        doc['process_status'] = 'failed-metadata';
+                        doc['process_message'] = 'Metadata process Failed, query failed.';
+                        // update the source document.
+                        axios.post(sourceUpdate, doc
+                        ).then(function(su3Res) {
+
+                        }).catch(function(su3Err) {
+
+                        });
                     });
+                }).catch(function(suErr) {
 
-                    // update the source document.
-                })
-                .catch(function(err) {
-                    console.log("Query Failed! - " + doc[localConfig.idField]);
-                    //console.dir(err);
-                    report();
                 });
+                // - if success update status to metadata processing done
+                // - if fail update status to metadata processing failed
+
             }, function() {
                 console.log(now() + " Async post done!");
                 reportDone(payload.length);
             });
-
-            /**
-             * ====================================================================
-             * the Promise.all try is not working yet.
-             * we will come back later.
-            let promises = response.data.response.docs.map(localConfig.mergeDoc);
-            Promise.all(promises).then((payload) => {
-
-                // async call
-                strategy.iterateOver(payload, function(doc, report) {
-                    axios.post(targetEndPoint, doc
-                    ).then(function(postRes) {
-                        //console.log("Post Success!");
-                        report();
-                        //console.dir(postRes);
-                    }).catch(function(postError) {
-                        console.log("Post Failed! - " + doc[localConfig.idField]);
-                        //console.dir(postError.data);
-                        // log the erorr and then report the copy is done!
-                        report();
-                    });
-                }, function() {
-                    console.log(now() + " Async post done!");
-                    reportDone(payload.length);
-                });
-            }).catch((error) => {
-                console.log("Promise Error");
-                console.log(error);
-            });
-             * =====================================================================
-             */
         })
         .catch(function(error) {
             // handle errors here.
@@ -178,13 +189,3 @@ axios.get(sourceSelect, totalQuery)
     console.log("Total Query Error!");
     console.dir(totalError);
 });
-
-//// preparing the solr query.
-//let solrQuery = {
-//    params: {
-//      q: "*:*",
-//      //fq: "version_schema:1.0",
-//      rows: 100
-//      //fl: "id,c4c_type,file_content,filehash,file_content_hash,file_size"
-//    }
-//};
