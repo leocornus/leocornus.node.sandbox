4ee8c59c258dbcce9cb936bd67434181df662126
Sean Chen <sean.chen@leocorn.com>
2019-01-30 17:47:14 +0000

    quick test to define function in a Node module

--- src/routes/drm.js
+++ src/routes/drm.js
@@ -2,13 +2,18 @@
 
 const config = require("../config");
 
+let logging = function(message) {
+
+    console.log(message);
+};
+
 module.exports = function(app) {
 
     // download file using the post protocol.
     app.post("/drm/download", function(req, res) {
 
         // check the body.
-        console.log(req.body);
+        logging(req.body);
         return res.send(req.body.name);
     });
 };
