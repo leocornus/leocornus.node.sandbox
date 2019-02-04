5c97010397ed1a52a323262cbc6d4343f70e47c5
Sean Chen <sean.chen@leocorn.com>
2019-02-04 14:10:25 +0000

    get ready the doc details for download service.

--- src/routes/drm.js
+++ src/routes/drm.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// using the UUID v4.
+const uuidv4 = require('uuid/v4');
 const config = require("../config");
 const Vitrium = require("../libs/vitrium");
 
@@ -11,23 +13,52 @@ let logging = function(message) {
 module.exports = function(app) {
 
     // download file using the post protocol.
-    //app.post("/drm/download", function(req, res) {
+    app.post("/drm/download", function(req, res) {
 
-    //    // check the body.
-    //    logging(req.body);
-    //    return res.send(req.body.name);
-    //});
+        // check the body.
+        logging(req.body);
+        return res.send(req.body.name);
+    });
 
     // quick test for download.
     app.get("/drm/download", function(req, res) {
+
+        // redirect to homepage.
+        res.redirect(301, config.server.homeRedirectUrl);
+    });
+
+    // quick test for download.
+    app.get("/drm/gdownload", function(req, res) {
 
         let vitrium = new Vitrium(
             config.vitrium.accountToken,
             config.vitrium.userName,
             config.vitrium.password
         );
+        let docDetails = {
+          "DocCode": config.vitrium.testData.docCodes[0],
+          "UserName": config.vitrium.testData.users[0],
+          "UniqueDocCopyId": uuidv4(),
+          "DocPolicyOverride": {
+            "PrintType": "HighResolution",
+            "AllowCopy": true,
+            "AllowBuildInLoginTemplate": true,
+            "AcroJsGosUnlimitedBehaviourType": "PromptAndCloseDocument",
+            "AcroJsGosBehaviorType": "PromptAndCloseDocument"
+          },
+          "AccessPolicyOverride": {
+            "RelativeExpiryInDays": null,
+            "OpenLimit": null,
+            "OfflineDurationinDays": 18250,
+            "IpAddressesMax": null,
+            "IgnoredIpAddresses": null,
+            "ExpiryInMins": 5256000,
+            "DocumentLimit": 10,
+            "ComputersMax": 2
+          }
+        };
 
-        vitrium.versionUnique(null, (uniqueRes, uniqueErr) => {
+        vitrium.versionUnique(docDetails, (uniqueRes, uniqueErr) => {
 
             console.log('unique respose header:');
             console.log(uniqueRes.headers);
