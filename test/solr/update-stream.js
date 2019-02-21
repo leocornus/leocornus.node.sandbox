f6c7e04c53bb1d96a42a9d4b0c238358b0aa5181
Sean Chen <sean.chen@leocorn.com>
2019-02-21 14:35:55 +0000

    the stream body delete by query test case

--- test/solr/update-stream.js
+++ test/solr/update-stream.js
@@ -6,3 +6,24 @@ const axios = require('axios');
 
 const config = require('./../../src/config');
 
+const localConfig = config.solrStream;
+
+// the update endpoint.
+const solrEndpoint = localConfig.baseUrl + "update";
+
+// set the stream body:
+let streamBody = {
+    params: {
+        "stream.body": `<delete><query>${localConfig.deleteQuery}</query></delete>`,
+        "commit": true
+    }
+};
+
+// simple get request.
+axios.get(solrEndpoint, streamBody)
+.then(function(res) {
+    console.log(res);
+})
+.catch(function(err) {
+    console.log(err);
+});
