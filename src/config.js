277ecec0da9603a89b646dd82fc2ee49b2a6a445
Sean Chen <sean.chen@leocorn.com>
2019-03-21 14:48:09 +0000

    here is the better way to check if a file is exist

--- src/config.js
+++ src/config.js
@@ -1,16 +1,10 @@
 /**
  * configuration for the application
  */
-const fs = require('fs');
-
 var config = {
     /**
      * here are some default values.
      */
-
-    // URL to SharePoint Online
-    spoUrl: "https://sites.sharepoint.com",
-    spoSite: "/sites/dev",
     username: 'someone@outlook.com',
     password: 'somepassword',
 
@@ -20,17 +14,16 @@ var config = {
 
 };
 
-// check if the local.js exist?
-if(fs.existsSync('./local.js')) {
-    // file not exist. 
-    console.log("Could not find local.js! Consider create one!");
-} else {
+try {
     const local = require('./local');
     // the first arg is the target Object,
     // all other objects are source objects.
     // The properties in source Object will override
     // the properties in target object.
     config = Object.assign(config, local);
+} catch (e) {
+    // file not exist.
+    console.log("Could not find src/local.js! Consider create one!");
 }
 
 module.exports = config;
