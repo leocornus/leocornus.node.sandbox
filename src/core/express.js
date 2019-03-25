fe490a4133fc2ae26fb475fefa76de98bac87875
Sean Chen <sean.chen@leocorn.com>
2019-03-25 17:48:12 +0000

    introduce the cors lib, allow any origin by default

--- src/core/express.js
+++ src/core/express.js
@@ -2,12 +2,16 @@
 
 let config = require("../config");
 let express = require("express");
+let cors = require("cors");
 let bodyParser = require("body-parser");
 
 module.exports = function() {
 
     // Create express app
     let app = express();
+    // set up CORS:
+    app.use(cors());
+
     // use body-parser as the middle-ware.
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
