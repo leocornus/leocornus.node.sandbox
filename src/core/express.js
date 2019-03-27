a1ead958de95ac925f4fe11be61bce96db217a22
Sean Chen <sean.chen@leocorn.com>
2019-03-27 14:12:58 +0000

    introduce the allowed origins config option

--- src/core/express.js
+++ src/core/express.js
@@ -10,7 +10,22 @@ module.exports = function() {
     // Create express app
     let app = express();
     // set up CORS:
-    app.use(cors());
+    app.use(cors({
+      origin: function(origin, callback){
+
+          // allow requests with no origin
+          // (like mobile apps or curl requests)
+          if(!origin) return callback(null, true);
+
+          if(config.server.allowedOrigins.indexOf(origin) === -1){
+              var msg = 'The CORS policy for this site does not ' +
+                        'allow access from the specified Origin.';
+              return callback(new Error(msg), false);
+          }
+
+          return callback(null, true);
+      }
+    }));
 
     // use body-parser as the middle-ware.
     app.use(bodyParser.urlencoded({ extended: false }));
