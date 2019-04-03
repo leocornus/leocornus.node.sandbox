ba4bf6e2699262ced7e9c15c14f092fe8868335f
Sean Chen <sean.chen@leocorn.com>
2019-04-03 11:36:23 +0000

    add some utility scripts

---
+++ util/process.sh
@@ -0,0 +1,5 @@
+# /usr/bin/bash
+
+# The path to node binary could be found by using the command:
+# nvm which node
+/home/csaadmin/.nvm/versions/node/v10.9.0/bin/node /opt/dev/node/test/solr/sync-queue-merge.js >> /opt/dev/node/merge-20190325.log
