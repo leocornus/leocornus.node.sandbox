8c50a1e4d926a942a072bb78889cb183c9210260
Sean Chen <sean.chen@leocorn.com>
2019-04-18 14:22:25 +0000

    add some logging message and more testing query.

--- test/jsforce/simple-test.js
+++ test/jsforce/simple-test.js
@@ -7,22 +7,26 @@ const jsforce = require('jsforce');
 const config = require('./../../src/config');
 
 let conn = new jsforce.Connection({
-    logLevel: "DEBUG",
+    //logLevel: "DEBUG",
     loginUrl: config.jsforce.authorizationUrl
 });
 
 conn.login(config.jsforce.username, 
-           //config.jsforce.password,
            config.jsforce.password + config.jsforce.securityToken,
-           //config.jsforce.securityToken,
            function(err, res) {
 
     if (err) {
         return console.error(err);
     }
 
+    console.log(res);
+
+    // test a simple query.
+    //let soql = 'SELECT Id, Name FROM Account';
+    let soql = config.jsforce.testingQuerys[0];
+
     // get more account information.
-    conn.query('SELECT Id, Name FROM Account', function(err, res) {
+    conn.query(soql, function(err, res) {
         if (err) {
             return console.error(err);
         }
