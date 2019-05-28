8ea891c149776df74a6158bc569748d988c6d242
Sean Chen <sean.chen@leocorn.com>
2019-05-28 18:02:54 +0000

    check the MD5 hash of a file in Azure storage

--- test/azure/azure-storage-stream.js
+++ test/azure/azure-storage-stream.js
@@ -1,3 +1,7 @@
+/**
+ * quick test to get the md5 hash for a file stored on Azure storage.
+ */
+
 const config = require('./../../src/config');
 const fs = require('fs');
 const crypto = require('crypto');
@@ -10,19 +14,29 @@ const localConfig = config.azure;
 var fileService = azure.createFileService(localConfig.storageAccount,
                                           localConfig.storageAccessKey);
 
-var hash = crypto.createHash('md5');
 // get file to stream.
 fileService.getFileToStream(localConfig.storageFileShare,
                             localConfig.testData[0].folder,
                             localConfig.testData[0].file,
-                            hash,
+                            fs.createWriteStream(localConfig.testData[0].file),
                             function(error, result, response) {
     if (!error) {
         // if result = true, share was created.
         // if result = false, share already existed.
         console.log("getFile----------------");
         console.dir(response);
-        // output the sum in hex format.
-        console.log(hash.digest('hex'));
+        console.log("Result: ");
+        console.dir(result);
+
+        var hash = crypto.createHash('md5');
+        var s = fs.ReadStream(localConfig.testData[0].file);
+        s.on('data', function(d) {
+              hash.update(d);
+        });
+
+        s.on('end', function() {
+              var d = hash.digest('hex');
+              console.log(d + '  ' + localConfig.testData[0].file);
+        });
     }
 });
