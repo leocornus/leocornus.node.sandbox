ce445f1df41fd28e7b2fb112240e76dc885e12ba
Sean Chen <sean.chen@leocorn.com>
2019-05-29 17:47:24 +0000

    add the tika/form request, reuse the same formdata object

--- test/tika/simple-request-test.js
+++ test/tika/simple-request-test.js
@@ -16,8 +16,9 @@ let formData = {
     file: fs.createReadStream(localConfig.testData[0].file)
 };
 
+// the request to get metadata.
 let metaReq = {
-    url: localConfig.baseUrl + 'meta/form', 
+    url: localConfig.baseUrl + 'meta/form',
     headers: {
         "Accept": "application/json",
         "Content-Type": "multipart/form-data"
@@ -30,5 +31,24 @@ request.post( metaReq, function(err, res, body) {
     console.dir(body);
     console.log("type of body: " + typeof(body));
     console.dir(JSON.parse(body));
+    //console.dir(res);
+} );
+
+// the request to get content text
+let tikaReq = {
+    url: localConfig.baseUrl + 'tika/form',
+    headers: {
+        "Accept": "text/plain",
+        "Content-Type": "multipart/form-data"
+    },
+    // we could not reuse the form data.
+    formData: formData
+};
+
+request.post( tikaReq, function(err, res, body) {
+
+    console.dir(body);
+    console.log("type of body: " + typeof(body));
+    //console.dir(JSON.parse(body));
     //console.dir(res);
 } );
