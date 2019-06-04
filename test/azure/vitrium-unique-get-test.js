66ecb62628afaf69af815dd93ebc3788f84bcfc6
Sean Chen <sean.chen@leocorn.com>
2019-06-04 17:47:52 +0000

    add the test case to use async and await

--- test/azure/vitrium-unique-get-test.js
+++ test/azure/vitrium-unique-get-test.js
@@ -9,6 +9,7 @@
  *  $ nvm run node test/azure/vitrium-policy-test.js
  */
 
+const axios = require('axios');
 // we should have separate local.js file for vitrium.
 const config = require('./../../src/config').vitrium;
 
@@ -20,7 +21,7 @@ let vitrium = new Vitrium(
     config.password,
 );
 
-// quick test to get all Folders.
+// quick test to get all unique docs for a given doc code..
 vitrium.getUniqueDocs(config.testData.docCodes[0],
                       config.testData.users[0],
                       // using the empty customField for now.
@@ -31,3 +32,25 @@ vitrium.getUniqueDocs(config.testData.docCodes[0],
     console.log(`unique id: ${res.data[0].Id}`);
     console.log(res.data[0]);
 });
+
+// using await wait the promise to be resolved.
+async function test() {
+
+    try {
+        let copyId = await vitrium.getUniqueDocs(
+                config.testData.docCodes[0],
+                config.testData.users[0],
+                // using the empty customField for now.
+                config.testData.customFields[0]);
+        
+        console.log( copyId.data.length );
+        console.log( `Existing unique copy id: ${copyId.data[0].Id}` );
+        return copyId;
+    } catch (err) {
+
+        console.error(err);
+    }
+}
+
+test()
+   // .then(console.log);
