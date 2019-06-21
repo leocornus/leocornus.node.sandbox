95fbf3dcc576348387aea913c9698a74561a6626
Sean Chen <sean.chen@leocorn.com>
2019-06-21 14:22:40 +0000

    try the basic digest authentication, got 502 error

--- test/crea/simple-login-test.js
+++ test/crea/simple-login-test.js
@@ -9,3 +9,20 @@ const config = require('./../../src/config');
 const localConfig = config.crea;
 
 console.log(localConfig.loginUrl);
+
+// CREA using basic digest auth to do authentication:
+var token = Buffer.from(localConfig.username + ':' + localConfig.password)
+            .toString('base64')
+axios.get(localConfig.loginUrl, {
+    headers:{
+        'Authorization': 'Basic ' + token 
+    }
+}).then(function(response) {
+      console.dir(response);
+      console.log('Authenticated');
+}).catch(function(error) {
+      console.dir(error);
+      console.log('Error on Authentication');
+});
+
+// TODO: got 502 Gateway error! need check more!
