4ab402b2a61275efa845a2dc514bde55b5b3fbb2
Sean Chen <sean.chen@leocorn.com>
2019-06-27 13:18:31 +0000

    make the successfully loged in through digest authentication

--- test/crea/deepdive-login-test.js
+++ test/crea/deepdive-login-test.js
@@ -31,19 +31,55 @@ axios.get(localConfig.loginUrl).then(function(response) {
     console.dir(error.response.headers);
 
     // get the authentication params
-    var authHeaders = error.response.headers['www-authenticate'];
-    var authParams = authHeaders.substring(7).split( ', ' ).
+    var challengeHeaders = error.response.headers['www-authenticate'];
+    var challengeParams = challengeHeaders.substring(7).split( ', ' ).
         // convert an arry to a Object.
         reduce( (params, item) => {
-            var parts = item.split('=');
-            params[parts[0]] = parts[1].replace(/"/g, '');
+
+            if( item.startsWith('nonce') ) {
+                var parts = item.split('nonce=');
+                params['nonce'] = parts[1].replace(/"/g, '');
+            } else {
+                var parts = item.split('=');
+                params[parts[0]] = parts[1].replace(/"/g, '');
+            }
             return params;
         }, {} );
-    console.dir(authParams);
+    console.dir(challengeParams);
+
+    // TODO: calculate Authenticate digest string.
+    var ha1 = md5(localConfig.username + ':' + 
+                  challengeParams.realm + ':' + localConfig.password)
+    var ha2 = md5('GET:' + localConfig.loginUrl)
+    var response = md5(ha1 + ':' + challengeParams.nonce + ':1::auth:' + ha2)
+    var authParams = {
+      username : localConfig.username,
+      realm : challengeParams.realm,
+      nonce : challengeParams.nonce,
+      uri : localConfig.loginUrl, 
+      qop : challengeParams.qop,
+      response : response,
+      nc : '1',
+      cnonce : '',
+    };
 
     // stringify the params:
     var authParamStr = Object.keys(authParams).reduce( (paramStr, key) => {
         return paramStr + ', ' + key + '="' + authParams[key] + '"';
     }, '' );
-    console.log('Digest ' + authParamStr.substring(2));
+    authParamStr = 'Digest ' + authParamStr.substring(2);
+    console.log(authParamStr);
+
+    axios.get(localConfig.loginUrl, {
+        headers: {
+            "Authorization": authParamStr
+        }
+    }).then(function(response) {
+        console.log("Success!");
+        //console.dir(response);
+        console.dir(response.headers);
+        console.dir(response.data);
+    }).catch(function(error) {
+        console.log("Failed!");
+    });
 });
