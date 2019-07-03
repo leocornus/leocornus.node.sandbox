fec077ae1d71f2a00226f78c432433b64a4ef0ac
Sean Chen <sean.chen@leocorn.com>
2019-07-03 18:47:26 +0000

    load the log4js configuration to enable the logging message

--- test/crea/crea-simple-test.js
+++ test/crea/crea-simple-test.js
@@ -3,8 +3,11 @@
  */
 
 const Crea = require('./../../src/libs/crea');
+const log4js = require('log4js');
 
 const config = require('./../../src/config');
+// configure log4js
+log4js.configure(config.crea.log4jsConfig);
 
 let crea = new Crea( config.crea.username, config.crea.password );
 
