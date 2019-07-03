2edd0ec5cf5022545033cb3ec2c9824a7b3b1a7c
Sean Chen <sean.chen@leocorn.com>
2019-07-03 17:47:24 +0000

    async authorize with dummy cookie

--- src/libs/crea.js
+++ src/libs/crea.js
@@ -8,7 +8,6 @@ const logger = require('log4js').getLogger('crea');
 
 const axios = require('axios');
 const md5 = require('crypto-js/md5');
-
 const fs = require('fs');
 
 /**
@@ -30,18 +29,24 @@ const Crea = function Crea (username, password) {
         'GetMetadata': this.apiBaseUrl + 'Metadata.svc/GetMetadata'
     };
 
+    this.username = username;
+    this.password = password; 
+
     // set the instance property.
-    this.realm = '';
-    this.nonce = '';
-    this.qop = '';
-    this.cookie = '';
+    this.cookie = null;
+    // cookie should have this following format.
+    // { realm: '',
+    //   nonce: '',
+    //   qop: '',
+    //   cookie: '' }
 
     // local file to store the API session token.
     this.cookieFilePath = '/tmp/' + md5(username + password);
 
-    // check the token age, create new one if it is expired.
+    // check the cookie age, create new one if it is expired.
     // set it as a promise object.
     this._authorized = this.authorize();
+
     logger.info( `Current session: ${this.cookie}` );
 };
 
@@ -55,4 +60,57 @@ Crea.prototype.authorize = async function() {
 
     let self = this;
 
-}
+    // using try catch block to make sure the async promise is fulfilled.
+    try {
+        // check the local cookie file
+        if(fs.existsSync( self.cookieFilePath )) {
+            // check the modified time to calculate the age.
+            let stats = fs.statSync(self.cookieFilePath);
+            let age = (new Date()).getTime() - stats.mtimeMs;
+            // if age is less than 1 hour, it is still valid.
+            if(age < 3600000) {
+                logger.info( 'Trying to use an existing cookie' );
+                // set cookie;
+                self.cookie = JSON.parse(fs.readFileSync( self.cookieFilePath, 'utf8' ));
+            }
+        }
+
+        // no cookie exists
+        if( self.cookie === null ) {
+            logger.info( "Trying to authenticate for account: " +
+                         self.username);
+            // set cookie;
+            self.cookie = await self.getCookie();
+        }
+
+        if( self.cookie === null ) {
+            // try again.
+            // TODO: set timeout!
+            const cookie = await self.getCookie();
+        } else {
+            logger.info("Successfully authenticated!");
+            // write the same tokens to update modified time.
+            fs.writeFileSync(self.cookieFilePath,
+                             JSON.stringify(self.cookie), 'utf8');
+        }
+
+        // return will fulfill the async function promise.
+        return self.cookie;
+    } catch( err ) {
+        logger.error('Failed to authorize: ', err);
+        throw Error(err);
+    }
+};
+
+/**
+ * get cookie
+ */
+Crea.prototype.getCookie = async function() {
+
+    return {
+        realm: 'CREA.Distribution',
+        nonce: 'NjM2OTc2NjkwNDg1NzYuMzo5YWZkMzNlZTRjZmZmYjJlNmE5MzNmNmZmODllZjdmYQ==',
+        qop: 'auth',
+        cookie: 'ASP.NET_SessionId=wlyeqx20x03rra0jpmdyryhc; X-SESSIONID=471835de-8583-4111-b815-a7c12de807de'
+    };
+};
