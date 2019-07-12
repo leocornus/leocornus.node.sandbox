b9a5428c6c40bb8a483d7a01dd1b7989ba744c9b
Sean Chen <Sean.Chen@leocorn.com>
2019-07-12 18:31:35 +0000

    i

---
+++ test/solr/xml-2-solr.js
@@ -0,0 +1,17 @@
+
+const xml2js = require('xml2js');
+const fs = require('fs');
+
+// load configuration
+const config = require('./../../src/config');
+const localConfig = config.xml2Solr;
+
+var parser = new xml2js.Parser();
+var fileName = localConfig.xmlFileFolder + '/products_0001_2570_to_430420.xml';
+fs.readFile(fileName, function(err, data) {
+    parser.parseString(data, function(err, result) {
+
+        console.dir(result.products.product[0]);
+        console.dir(result.products.product[0].details[0].detail[0]);
+    });
+});
