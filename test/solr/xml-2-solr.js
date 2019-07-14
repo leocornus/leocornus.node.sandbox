3517c94fad4f77659a66b0505fe64339101f76dd
Sean Chen <Sean.Chen@leocorn.com>
2019-07-14 11:55:12 +0000

    load all files from a folder

--- test/solr/xml-2-solr.js
+++ test/solr/xml-2-solr.js
@@ -3,35 +3,47 @@ const axios = require('axios');
 const xml2js = require('xml2js');
 const fs = require('fs');
 
+const strategy = require('./../../src/libs/strategy');
 // load configuration
 const config = require('./../../src/config');
 const localConfig = config.xml2Solr;
 const solrEndPoint = localConfig.solrBaseUrl + "update/json/docs?commit=true";
 
 var parser = new xml2js.Parser();
-var fileName = localConfig.xmlFileFolder + '/products_0001_2570_to_430420.xml';
-fs.readFile(fileName, function(err, data) {
-    parser.parseString(data, function(err, result) {
+let files = fs.readdirSync(localConfig.xmlFileFolder);
+console.log(`Found ${files.length} files`);
 
-        console.log('Found ' + result.products.product.length + ' products');
-        let docs = result.products.product.map( item => {
+//var fileName = localConfig.xmlFileFolder + '/products_0001_2570_to_430420.xml';
+strategy.waterfallOver(0, files.length,
+    function(start, reportDone) {
 
-            return localConfig.prepareSolrDoc(item);
-        });
+    let fileName = localConfig.xmlFileFolder + "/" + files[start];
 
-        console.log(docs.length);
-        console.dir(docs[0]);
-        //console.dir(result.products.product[0]);
-        //console.dir(result.products.product[0].details[0].detail[0]);
-
-        // post to Solr collection
-        axios.post(solrEndPoint, docs
-        ).then(function(postRes) {
-            console.log("Post Success!");
-            //console.dir(postRes);
-        }).catch(function(postError) {
-            console.log("Post Fail");
-            console.dir(postError.response.data);
+    fs.readFile(fileName, function(err, data) {
+        parser.parseString(data, function(err, result) {
+    
+            console.log('- Found ' + result.products.product.length + ' products');
+            let docs = result.products.product.map( item => {
+    
+                return localConfig.prepareSolrDoc(item);
+            });
+    
+            //console.log(docs.length);
+            //console.dir(docs[0]);
+            //console.dir(result.products.product[0]);
+            //console.dir(result.products.product[0].details[0].detail[0]);
+    
+            // post to Solr collection
+            axios.post(solrEndPoint, docs
+            ).then(function(postRes) {
+                console.log("-- Post Success!");
+                //console.dir(postRes);
+                reportDone(1);
+            }).catch(function(postError) {
+                console.log("-- Post Fail");
+                //console.dir(postError.response.data);
+                reportDone(1);
+            });
         });
     });
 });
