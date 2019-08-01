96a9e0dfb83685d670a4f7068c5666e74807ec55
Sean Chen <sean.chen@leocorn.com>
2019-08-01 00:04:53 +0000

    move the test code to a separate file.

--- src/routes/drm-test.js
+++ src/routes/drm-test.js
@@ -7,22 +7,26 @@ const logger = require('log4js').getLogger('drm');
 
 module.exports = function(app) {
 
-    // download file using the post protocol.
-    app.post("/drm/download", function(req, res) {
-
-        // check the body.
-        logger.debug("request payload: ", req.body);
+    // quick test for download.
+    app.get("/drm/gdownload", function(req, res) {
 
         let vitrium = new Vitrium(
             config.vitrium.accountToken,
             config.vitrium.userName,
-            config.vitrium.password,
-            config.vitrium.formId
+            config.vitrium.password
         );
 
-        // the req.body will the payload.
-        vitrium.versionUnique(req.body, (uniqueRes, uniqueErr) => {
+        let docReq = {
+          "DocCode": config.vitrium.testData.docCodes[0],
+          "UserName": config.vitrium.testData.users[0],
+          "UserSetType": 'Retail',
+          "DocExpiryDate": '2069-12-31'
+        };
 
+        vitrium.versionUnique(docReq, (uniqueRes, uniqueErr) => {
+
+            //console.log(uniqueRes);
+            //console.log(uniqueErr);
             logger.debug('Unique respose headers: ', uniqueRes.headers);
 
             // set header from the unique response.
@@ -35,12 +39,5 @@ module.exports = function(app) {
             // the data is a stream.
             uniqueRes.data.pipe(res);
         });
-    });
-
-    // quick test for download.
-    app.get("/drm/download", function(req, res) {
-
-        // redirect to homepage.
-        res.redirect(301, config.server.homeRedirectUrl);
     });
 };
