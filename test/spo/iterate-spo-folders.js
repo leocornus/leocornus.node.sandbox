e7a98979be707dc0d48c2689c8ff7855e7321a01
Sean Chen <sean.chen@leocorn.com>
2019-10-06 00:16:16 +0000

    using iterateOver to process each folder.

--- test/spo/iterate-spo-folders.js
+++ test/spo/iterate-spo-folders.js
@@ -43,25 +43,22 @@ spoAuth.getAuth(spoConfig.spoUrl,
         // set up the iterator.
         let iterator = function(folder, report) {
             if( folder.Name.startsWith("Customer") ){
-                processRootFolder(headers, folder.Name);
+                processRootFolder(headers, folder.Name, report);
             } else {
-                report(folder.Name, 'Skip');
+                report();
             }
         };
 
         strategy.iterateOver(rootFolders, iterator, 
-            /**
-             * the report funciton.
-             * 
-             */
-            function(folderName, action, result) {
-                console.log(`Folder: ${folder.Name} - ${action} - ${result}`);
+            // complete callback.
+            function() {
+                //console.log(`Root folder complete!`);
             }
         );
     })
     .catch(function(siteError) {
         console.log("Failed to process site!");
-        console.dir(siteError);
+        console.log(siteError.Error);
     });
 });
 
@@ -69,7 +66,7 @@ spoAuth.getAuth(spoConfig.spoUrl,
  * process the root folder
  * the folder right under the site.
  */
-function processRootFolder(headers, rootFolder) {
+function processRootFolder(headers, rootFolder, report) {
 
     // root folder group folder.
     let reqRoot = {
@@ -84,62 +81,111 @@ function processRootFolder(headers, rootFolder) {
     axios.request(reqRoot).then(function(response) {
 
         // TODO: what's the limit of the Folders API?
-        let folders = response.data.value;
+        let cFolders = response.data.value;
 
-        //console.log(folders[0]);
-        //console.log('Found Folders: ' + folders.length);
-        folders.forEach((folder) => {
-            //console.log(folder.Name);
+        // set the iterator.
+        let cIterator = function(cFolder, cReport) {
+            processCFolder(headers, cFolder, cReport);
+        };
 
-            // processing level 1 folder [CUSTOMER FOLDER]
-            let reqOne = {
-                url: spoConfig.spoUrl + spoConfig.spoSite +
-                     "/_api/web/getfolderbyserverrelativeurl('" +
-                     encodeURIComponent(folder.ServerRelativeUrl) + "')/Folders",
-                method: "get",
-                headers: headers
-            };
-            axios.request(reqOne).then(function(oneRes) {
-
-                // processing level 2 folder [PROJECT FOLDER]
-                let pFolders = oneRes.data.value;
-                //console.log('Found Folders: ' + pFolders.length);
-                pFolders.forEach((folder) => {
-                    //console.log("-- " + folder.Name);
-                    if(folder.Name === "Certified Products") {
-
-                        let reqFiles = {
-                            url: spoConfig.spoUrl + spoConfig.spoSite +
-                                 "/_api/web/getfolderbyserverrelativeurl('" +
-                                 encodeURIComponent(folder.ServerRelativeUrl) + "')/Files",
-                            method: "get",
-                            headers: headers
-                        };
-
-                        axios.request(reqFiles).then(function(filesRes) {
-
-                            let files = filesRes.data.value;
-                            files.forEach( file => {
-                                //console.log(file.Name);
-                                console.log(file.ServerRelativeUrl);
-                            });
-                        })
-                        .catch(function(fileErr) {
-                            console.log(`Failed to process files: ${folder.ServerRelativeUrl}`);
-                            //console.dir(fileErr);
-                        });
-                    }
-                });
-            })
-            .catch(function(oneErr) {
-                console.log(`Failed to process customer folder: ${folder.ServerRelativeUrl}`);
-                //console.dir(oneErr);
-            });
-        });
-        //console.log(folders[0]);
+        strategy.iterateOver(cFolders, cIterator,
+            // complete call back..
+            function() {
+                //console.log(`cFolders complete!`);
+                report();
+            }
+        );
     })
     .catch(function(rootErr) {
-        console.log(`Failed to process root folder: ${folder.ServerRelativeUrl}`);
+        //console.log(`Failed to process root folder: ${folder.ServerRelativeUrl}`);
         //console.dir(rootErr);
+        console.log(rootErr.Error);
+        report();
     });
+}
+
+/**
+ * process c folder.
+ */
+function processCFolder(headers, folder, report) {
+
+    // processing level 1 folder [CUSTOMER FOLDER]
+    let reqOne = {
+        url: spoConfig.spoUrl + spoConfig.spoSite +
+             "/_api/web/getfolderbyserverrelativeurl('" +
+             encodeURIComponent(folder.ServerRelativeUrl) + "')/Folders",
+        method: "get",
+        headers: headers
+    };
+    axios.request(reqOne).then(function(oneRes) {
+
+        // processing level 2 folder [PROJECT FOLDER]
+        let pFolders = oneRes.data.value;
+
+        // set the iterator.
+        let pIterator = function(pFolder, pReport) {
+            processPFolder(headers, pFolder, pReport);
+        };
+
+        strategy.iterateOver(pFolders, pIterator,
+            // complete call back..
+            function() {
+                //console.log(`pFolders complete!`);
+                report();
+            }
+        );
+    })
+    .catch(function(oneErr) {
+        //console.log(`Failed to process c folder: ${folder.ServerRelativeUrl}`);
+        //console.dir(oneErr);
+        console.log(oneErr.Error);
+        report();
+    });
+}
+
+/**
+ * process p folder.
+ */
+function processPFolder(headers, folder, report) {
+
+    //console.log("-- " + folder.Name);
+    if(folder.Name === "Certified Products") {
+
+        let reqFiles = {
+            url: spoConfig.spoUrl + spoConfig.spoSite +
+                 "/_api/web/getfolderbyserverrelativeurl('" +
+                 encodeURIComponent(folder.ServerRelativeUrl) + "')/Files",
+            method: "get",
+            headers: headers
+        };
+
+        axios.request(reqFiles).then(function(filesRes) {
+
+            let files = filesRes.data.value;
+
+            // set iterator.
+            let fIterator = function(file, fReport) {
+                //console.log(file.Name);
+                console.log(file.ServerRelativeUrl);
+                fReport();
+            }
+
+            strategy.iterateOver(files, fIterator, 
+                // complet call back,
+                function() {
+                    //console.log(`Files complete: ${folder.ServerRelativeUrl}`);
+                    report();
+                }
+            );
+        })
+        .catch(function(fileErr) {
+            //console.log(`Failed to process files: ${folder.ServerRelativeUrl}`);
+            //console.dir(fileErr);
+            console.log(fileErr.Error);
+            report();
+        });
+    } else {
+        //console.log(`Skip folder: ${folder.ServerRelativeUrl}`);
+        report();
+    }
 }
