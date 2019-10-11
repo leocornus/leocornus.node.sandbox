5bf183a1da3ce54266acd0849afa4e37ba8363d0
Sean Chen <sean.chen@leocorn.com>
2019-10-11 17:52:24 +0000

    update to automatically generate a series of files

--- test/attivio/csv-report.js
+++ test/attivio/csv-report.js
@@ -4,6 +4,7 @@
 
 const axios = require('axios');
 const stringify = require('csv-stringify');
+const strategy = require('./../../src/libs/strategy');
 const fs = require('fs');
 
 // we using the solrCSVExport section of the local config.
@@ -25,30 +26,68 @@ let reportQuery = {
     query: reportSettings.queryParams.q,
     sort: reportSettings.queryParams.sort,
     offset: reportSettings.queryParams.start,
-    rows: reportSettings.queryParams.rows,
+    //rows: reportSettings.queryParams.rows,
+    rows: 1,
     searchProfile: "checkcity",
     //queryLanguage: "simple",
     queryLanguage: "advanced"
 };
 //console.log(reportQuery);
 
-// simple get request.
+// found out the total items.
 axios.post(attivioEndpoint, reportQuery)
 .then(function(res) {
     //console.log(res);
-    console.log('Total docs found: ' + res.data.totalHits);
+    let total = res.data.totalHits;
+    console.log(`Total docs found: ${total}`);
 
-    let payload = res.data.documents.map(function(doc) {
+    // calculate the files needed
+    let fileAmount = Math.ceil(total / reportSettings.itemsPerFile);
 
-        // final touch for each doc.
-        return doc.fields;
-    });
+    strategy.waterfallOver(0, fileAmount,
+        // the iterator function.
+        function(index, reportDone) {
 
-    let fileStream = fs.createWriteStream(reportSettings.csvFile);
-    stringify(payload, {
-        header: true,
-        columns: reportSettings.csvColumns
-    }).pipe(fileStream);
+            // get ready the file name.
+            let filePath = `${reportSettings.csvFile}-${index}.csv`;
+
+            // set the query offset.
+            reportQuery.offset = index * reportSettings.itemsPerFile;
+            reportQuery.rows = reportSettings.itemsPerFile;
+
+            console.log(`report query: ${reportQuery}`);
+
+            // query and generate the CSV file.
+            axios.post(attivioEndpoint, reportQuery)
+            .then(function(oneRes) {
+
+                let payload = oneRes.data.documents.map(function(doc) {
+
+                    // final touch for each doc.
+                    return doc.fields;
+                });
+
+                console.log(`Generating file: ${filePath}`);
+                let fileStream = fs.createWriteStream(filePath);
+                stringify(payload, {
+                    header: true,
+                    columns: reportSettings.csvColumns
+                }).pipe(fileStream);
+
+                reportDone(1);
+            })
+            .catch(function(oneErr) {
+                console.log(`Failed process ${index}`);
+                console.dir(oneErr);
+                // reportDone to skip.
+                reportDone(1);
+            });
+        },
+        // the completion callback.
+        function() {
+            console.log("All Done!");
+        });
+
 })
 .catch(function(err) {
     console.dir(err.response.data);
