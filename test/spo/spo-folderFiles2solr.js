816c1b916920574a37eb483fddadb8f925bf80c1
Sean Chen <sean.chen@leocorn.com>
2019-10-17 12:14:07 +0000

    update to do the SPO authentication first

--- test/spo/spo-folderFiles2solr.js
+++ test/spo/spo-folderFiles2solr.js
@@ -25,6 +25,22 @@ const targetEndPoint = localConfig.targetBaseUrl + "update/json/docs?commit=true
 console.log("From: " + solrEndpoint);
 console.log("To: " + targetEndPoint);
 
+// complete authentication and get the header
+spoAuth.getAuth(spoConfig.spoUrl,
+            {username: spoConfig.username,
+             password: spoConfig.password})
+.then(options => {
+
+    // let's check the options.
+    // it only contains a cookie which will have the
+    // access token.
+    //console.dir(options);
+
+    // get ready header.
+    let headers = options.headers;
+    //headers['Accept'] = 'application/json;odata=verbose';
+    headers['Accept'] = 'application/json';
+
 // simple query to get total number:
 let totalQuery = {
     params: {
@@ -41,134 +57,117 @@ axios.get(solrEndpoint, totalQuery)
 .then(function(totalRes) {
 
     let amount = totalRes.data.response.numFound;
-    console.log(`Total Docs: ${amount}`);
-
+    console.log(`Total Folders: ${amount}`);
     let docs = totalRes.data.response.docs;
+
+    // define the iterator to go through each folder.
     let iterator = function(index, reportOne) {
+
         let theFolder = localConfig.getFolder(docs[index]);
         //console.log(theFolder);
 
-        // load all files for the folder from SPO site.
-        spoAuth.getAuth(spoConfig.spoUrl,
-                    {username: spoConfig.username,
-                     password: spoConfig.password})
-        .then(options => {
+        let theUrl = spoConfig.spoUrl + spoConfig.spoSite +
+            "/_api/web/GetFolderByServerRelativeUrl('" +
+            encodeURIComponent(theFolder) + "')/Files";
+        //console.log(theUrl);
 
-            // let's check the options.
-            // it only contains a cookie which will have the
-            // access token.
-            //console.dir(options);
+        // prepare the axios request config.
+        let reqConfig = {
+          url: theUrl,
+          method: "get",
+          headers: headers,
+        };
 
-            // get ready header.
-            let headers = options.headers;
-            //headers['Accept'] = 'application/json;odata=verbose';
-            headers['Accept'] = 'application/json';
+        // call the API to get response.
+        axios.request(reqConfig).then(function(response) {
+            // dir will show up proper indention for a JSON
+            // object
+            // all files will be list in array named value.
+            let files = response.data.value;
+            //console.dir(response.data.value);
+            console.log(`Got ${files.length} files for folder ${theFolder}` );
 
-            let theUrl = spoConfig.spoUrl + spoConfig.spoSite +
-                "/_api/web/GetFolderByServerRelativeUrl('" +
-                encodeURIComponent(theFolder) + "')/Files";
-            //console.log(theUrl);
+            // --- for quick test
+            // quick test for one file.
+            //processOneFile(headers, folderName, theUrl, files[1].Name);
 
-            // prepare the axios request config.
-            let reqConfig = {
-              url: theUrl,
-              method: "get",
-              headers: headers,
-            };
+            // --- pring all file's ServerRelativeUrl for testing..
+            // forEach will send the requests all at once!
+            // it will be overwhelmed for large dataset.
+            //files.forEach((file) => {
+            //    console.log(file.ServerRelativeUrl);
+            //});
 
-            // call the API to get response.
-            axios.request(reqConfig).then(function(response) {
-                // dir will show up proper indention for a JSON
-                // object
-                // all files will be list in array named value.
-                let files = response.data.value;
-                //console.dir(response.data.value);
-                console.log(`Got ${files.length} files for folder ${theFolder}` );
+            if(files.length < 1) {
+                reportOnde(1);
+            } else {
+                // -- preparing payload for solr.
+                let docs = localConfig.prepareSolrDocs(files);
 
-                // --- for quick test
-                // quick test for one file.
-                //processOneFile(headers, folderName, theUrl, files[1].Name);
+                // TODO: check if the files are exist or not.
+                let sourceIds = docs.map(doc => {
+                    return doc[localConfig.idField];
+                });
+                //console.log(sourceIds.join('","'));
 
-                // --- pring all file's ServerRelativeUrl for testing..
-                // forEach will send the requests all at once!
-                // it will be overwhelmed for large dataset.
-                //files.forEach((file) => {
-                //    console.log(file.ServerRelativeUrl);
-                //});
-
-                if(files.length < 1) {
-                    reportOnde(1);
-                } else {
-                    // -- preparing payload for solr.
-                    let docs = localConfig.prepareSolrDocs(files);
-
-                    // TODO: check if the files are exist or not.
-                    let sourceIds = docs.map(doc => {
-                        return doc[localConfig.idField];
-                    });
-                    //console.log(sourceIds.join('","'));
-
-                    let queryExist = {
-                        query: localConfig.idField + ":(\"" + sourceIds.join('\",\"') + "\")",
-                        params: {
-                            // we need the "" for list of ids.
-                            rows: docs.length,
-                            fl: [localConfig.idField]
-                        }
+                let queryExist = {
+                    query: localConfig.idField + ":(\"" + sourceIds.join('\",\"') + "\")",
+                    params: {
+                        // we need the "" for list of ids.
+                        rows: docs.length,
+                        fl: [localConfig.idField]
                     }
-                    //console.log(queryExist);
-
-                    axios.post(targetQEndPoint, queryExist)
-                    .then(function(existRes) {
-
-                        let existDocs = existRes.data.response.docs;
-                        if(existDocs.length === docs.length) {
-                            console.log(` - All files are exist, SKIP!`);
-                            reportOne(1);
-                        } else {
-                            let existIds = existDocs.map(doc => {
-                                return doc[localConfig.idField];
-                            });
-                            // remove found ids.
-                            let payload = docs.map(doc => {
-                                if(! existIds.includes(doc[localConfig.idField])) {
-                                    // return not exist ids.
-                                    return doc;
-                                }
-                            });
-                            axios.post(targetEndPoint, payload
-                            ).then(function(postRes) {
-                                console.log(` - Post Success: ${payload.length} ${theFolder}`);
-                                // report one folder complete.
-                                reportOne(1);
-                                //console.dir(postRes);
-                            
-                            }).catch(function(postError) {
-                                console.log(` - Post Failed! ${payload.length} ${theFolder}`);
-                                //console.dir(postError.data);
-                                // log the erorr and then report the copy is done!
-                                reportOne(1);
-                            });
-                        }
-                    })
-                    .catch(function(existErr) {
-                        console.log("Exist Query Failed!");
-                        console.error(existErr);
-                        // report one folder complete.
-                        reportOne(1);
-                    });
                 }
-            })
-            .catch(function(resErr) {
-                console.log(`Failed to get files for folder ${theFolder}`);
-                // report folder complete.
-                reportOne(1);
-            });
-        }).catch(error => {
-            console.dir(error);
-        });
+                //console.log(queryExist);
 
+                axios.post(targetQEndPoint, queryExist)
+                .then(function(existRes) {
+
+                    let existDocs = existRes.data.response.docs;
+                    if(existDocs.length === docs.length) {
+                        console.log(` - All files are exist, SKIP!`);
+                        reportOne(1);
+                    } else {
+                        let existIds = existDocs.map(doc => {
+                            return doc[localConfig.idField];
+                        });
+                        // remove found ids.
+                        let payload = docs.map(doc => {
+                            if(! existIds.includes(doc[localConfig.idField])) {
+                                // return not exist ids.
+                                return doc;
+                            }
+                        });
+                        axios.post(targetEndPoint, payload
+                        ).then(function(postRes) {
+                            console.log(` - Post Success: ${payload.length} ${theFolder}`);
+                            // report one folder complete.
+                            reportOne(1);
+                            //console.dir(postRes);
+                        }).catch(function(postError) {
+                            console.log(` - Post Failed! ${payload.length} ${theFolder}`);
+                            //console.dir(postError.data);
+                            // log the erorr and then report the copy is done!
+                            reportOne(1);
+                        });
+                    }
+                })
+                .catch(function(existErr) {
+                    console.log("Exist Query Failed!");
+                    console.log(existErr);
+                    console.dir(existErr);
+                    // report one folder complete.
+                    reportOne(1);
+                });
+            }
+        })
+        .catch(function(resErr) {
+            console.log(`Failed to get files for folder ${theFolder}`);
+            // report folder complete.
+            reportOne(1);
+        });
     };
+
     strategy.waterfallOver(0, docs.length, iterator,
         // all complete!
         function() {
@@ -185,4 +184,10 @@ axios.get(solrEndpoint, totalQuery)
 
     console.log("Total Query Error!");
     console.dir(totalErr);
+});
+
+// end of SPO authentication.
+}).catch(error => {
+    // failed to authenticated from SPO.
+    console.dir(error);
 });
