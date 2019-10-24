506d3cf93d75df9c33607a924d39f8a2957470f9
Sean Chen <sean.chen@leocorn.com>
2019-10-24 14:34:10 +0000

    get started the test case to read binary from HTTP response

---
+++ test/spo/test-sp-file.js
@@ -0,0 +1,56 @@
+/**
+ * quick test to use node-sp-auth to connect to SPO
+ * and then download files.
+ * 
+ * execute the test by using the following command:
+ *
+ *  nvm run node test/test-sp-file.js
+ */
+
+// we have to use the ./ as current foler.
+const config = require('./../../src/config');
+const spo = require('node-sp-auth');
+const request = require('request');
+//const request = require('request-promise');
+
+//console.log(JSON.stringify(config, null, 2));
+const configSPO = config.spo;
+
+spo.getAuth(configSPO.spoUrl, {username: configSPO.username,
+                            password: configSPO.password})
+.then(options => {
+    // let's check the options.
+    // it only contains a cookie which will have the
+    // access token.
+    console.dir(options);
+
+    // perform request with any http-enabled library 
+    // (request-promise in a sample below):
+    let headers = options.headers;
+    //headers['Accept'] = 'application/json;odata=verbose';
+    headers['Accept'] = 'application/json';
+
+    // this is the sample to show hte /Folders API.
+    filePath = configSPO.samplePathes[0];
+    // this is the sample to show hte /Properites API.
+    //filePath = configSPO.samplePathes[1];
+    // this is the sample to show the /Files API.
+    //filePath = configSPO.samplePathes[3];
+    // construct the URL.
+    let theUrl = configSPO.spoUrl + configSPO.spoSite + filePath;
+    console.log(theUrl);
+
+    request.get({
+        requestContentType: 'JSON',
+        url: `${theUrl}`,
+        headers: headers
+    // the way for request.
+    }, function(error, response, body) {
+    // the way for request promise.
+    //}).then(function(error, response, body) {
+        //process data
+        //console.dir(response);
+        console.dir(body);
+        //console.dir(JSON.parse(body));
+    });
+});
