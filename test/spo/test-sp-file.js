2297df365b4b7d12ccd443a573ce054f605e47b6
Sean Chen <sean.chen@leocorn.com>
2019-10-24 15:25:35 +0000

    add the function to show how request lib handle binary stream

--- test/spo/test-sp-file.js
+++ test/spo/test-sp-file.js
@@ -4,53 +4,73 @@
  * 
  * execute the test by using the following command:
  *
- *  nvm run node test/test-sp-file.js
+ *  nvm run node test/spo/test-sp-file.js
  */
 
+const fs = require('fs');
 // we have to use the ./ as current foler.
 const config = require('./../../src/config');
 const spo = require('node-sp-auth');
 const request = require('request');
 //const request = require('request-promise');
+const axios = require('axios');
 
 //console.log(JSON.stringify(config, null, 2));
 const configSPO = config.spo;
 
-spo.getAuth(configSPO.spoUrl, {username: configSPO.username,
-                            password: configSPO.password})
-.then(options => {
-    // let's check the options.
-    // it only contains a cookie which will have the
-    // access token.
-    console.dir(options);
+requestStream();
 
-    // perform request with any http-enabled library 
-    // (request-promise in a sample below):
-    let headers = options.headers;
-    //headers['Accept'] = 'application/json;odata=verbose';
-    headers['Accept'] = 'application/json';
+/**
+ * using request lib to handle binary file as stream.
+ */
+function requestStream() {
 
-    // this is the sample to show hte /Folders API.
-    filePath = configSPO.samplePathes[0];
-    // this is the sample to show hte /Properites API.
-    //filePath = configSPO.samplePathes[1];
-    // this is the sample to show the /Files API.
-    //filePath = configSPO.samplePathes[3];
-    // construct the URL.
-    let theUrl = configSPO.spoUrl + configSPO.spoSite + filePath;
-    console.log(theUrl);
+    let localFile = '/tmp/test.pdf';
+    let output = fs.createWriteStream(localFile);
 
-    request.get({
-        requestContentType: 'JSON',
-        url: `${theUrl}`,
-        headers: headers
-    // the way for request.
-    }, function(error, response, body) {
-    // the way for request promise.
-    //}).then(function(error, response, body) {
-        //process data
-        //console.dir(response);
-        console.dir(body);
-        //console.dir(JSON.parse(body));
+    spo.getAuth(configSPO.spoUrl, {username: configSPO.username,
+                                password: configSPO.password})
+    .then(options => {
+        // let's check the options.
+        // it only contains a cookie which will have the
+        // access token.
+        console.dir(options);
+
+        // perform request with any http-enabled library
+        // (request-promise in a sample below):
+        let headers = options.headers;
+        //headers['Accept'] = 'application/json;odata=verbose';
+        headers['Accept'] = 'application/json';
+
+        // this is the sample to show hte /Folders API.
+        filePath = configSPO.samplePathes[0];
+        // this is the sample to show hte /Properites API.
+        //filePath = configSPO.samplePathes[1];
+        // this is the sample to show the /Files API.
+        //filePath = configSPO.samplePathes[3];
+        // construct the URL.
+        let theUrl = configSPO.spoUrl + configSPO.spoSite + filePath;
+        console.log(theUrl);
+
+        let req = request.get({
+            requestContentType: 'JSON',
+            url: `${theUrl}`,
+            headers: headers
+        // the way for request.
+        });
+
+        // here is how to handle the binary file as stream.
+        req.on('response', (response) => {
+            //process data
+            //console.dir(response);
+            console.log('Start download using stream...');
+            req.pipe(output);
+            console.log('DONE Setup pipe...');
+            //console.dir(body);
+        });
+
+        req.on('end', (res) => {
+            console.log('download complete');
+        });
     });
-});
+}
