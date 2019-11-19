76d0bf815aeb7544294fa0c9937de5b1a00c6f5c
Sean Chen <sean.chen@leocorn.com>
2019-11-19 17:27:44 +0000

    try the prettyMs to format time difference

--- test/core/datetime-test.js
+++ test/core/datetime-test.js
@@ -1,6 +1,8 @@
 /**
  */
 
+const prettyMs = require('pretty-ms');
+
 // some strings:
 let dString = "2019-11-19 15:50:14 063";
 //let theDate = Date.parse(dString);
@@ -12,6 +14,12 @@ let dStringOne = "Tue Nov 19 07:23:58 PST 2019";
 let theDateOne = new Date(dStringOne);
 console.log(dStringOne, "to", theDateOne, "ISO:", theDateOne.toISOString());
 
+console.log("Difference (ms):", (theDate - theDateOne));
+console.log("Day on Market:", prettyMs(theDate - theDateOne));
+
 let dString3 = "Fri May 06 09:25:41 PDT 2016";
 let theDate3 = new Date(dString3);
 console.log(dString3, "to", theDate3, "ISO:", theDate3.toISOString());
+
+console.log("Difference (ms):", (theDate - theDate3));
+console.log("Day on Market:", prettyMs(theDate - theDate3));
