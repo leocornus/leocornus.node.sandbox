38b29b7869cf56ab256b341ac0be43b8dd3d4ad9
Sean Chen <sean.chen@leocorn.com>
2019-12-08 21:09:10 +0000

    fix the one day in ms.

--- test/core/datetime-test.js
+++ test/core/datetime-test.js
@@ -3,7 +3,9 @@
 
 const prettyMs = require('pretty-ms');
 // one day in ms, as the
-const ONE_DAY = 24 * 60 * 60 * 60 * 1000;
+const ONE_DAY = 24 * 60 * 60 * 1000;
+
+console.log("One Day (ms):", ONE_DAY);
 
 // some strings:
 let dString = "2019-11-19 15:50:14 063";
@@ -18,6 +20,10 @@ console.log(dStringOne, "to", theDateOne, "ISO:", theDateOne.toISOString());
 
 console.log("Difference (ms):", (theDate - theDateOne));
 console.log("Difference (Day):", Math.ceil((theDate - theDateOne) / ONE_DAY));
+let today = new Date();
+console.log("Today:", today);
+console.log("Difference to today (ms):", (today - theDateOne));
+console.log("Difference to today (Day):", Math.ceil((today - theDateOne) / ONE_DAY));
 console.log("Day on Market:", prettyMs(theDate - theDateOne));
 
 let dString3 = "Fri May 06 09:25:41 PDT 2016";
