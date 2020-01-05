8952a23cda9dc701b5887068f7a2695efafb9cec
Sean Chen <Sean.Chen@leocorn.com>
2020-01-05 17:49:38 -0500

    introduce the separator.

--- test/core/string-title-case-test.js
+++ test/core/string-title-case-test.js
@@ -7,21 +7,37 @@
 /**
  * utility function to title case the given string.
  */
-function titleCase(str) {
+function titleCase(str, separator) {
+
+    // set the default separator to ' ';
+    separator = separator ? separator : ' ';
 
     // make it lower case first.
     return str.toLowerCase()
     // split the string to an array of strings.
-              .split(' ')
+              .split(separator)
               .map(word => {
+
+                  if(word.includes('/')) {
+                      word = titleCase(word, '/');
+                  }
+
+                  if(word.includes('-')) {
+                      word = titleCase(word, '-');
+                  }
+
                   return word.charAt(0).toUpperCase() + word.slice(1);
               })
-              .join(' ');
+              .join(separator);
 }
 
 let theStrings = [ 
     "i'm a little tea pot",
-    "i'm a little tea pot with & sign"
+    "i'm a little tea pot with & sign",
+    "the city name Alnwick/Haldimand",
+    "another city name Whitchurch-Stouffville",
+    "more interesting name Assiginack, Manitoulin Is",
+    "name with bracket Adelaide Metcalfe (Twp)"
 ];
 
 theStrings = theStrings.map(one => {
