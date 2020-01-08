ea45182ef3959bd61ca04930f292f83059256142
Sean Chen <Sean.Chen@leocorn.com>
2020-01-08 09:58:14 -0500

    add more cases.

--- test/core/array-reduce-test.js
+++ test/core/array-reduce-test.js
@@ -9,3 +9,15 @@ let case1 = {
 case1['total'] = case1['numbers'].reduce( (total, n) => total + n );
 
 console.table(case1);
+
+// simple case with initial value.
+case1['total'] = case1['numbers'].reduce( function(total, n) {
+    return total + n;
+}, 10); // set initial value to 0
+
+console.table(case1);
+
+// simple case with initial value.
+case1['total'] = case1['numbers'].reduce( (total, n) => total + n, 20);
+
+console.table(case1);
