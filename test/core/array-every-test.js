a46a82ce4f4761d0aa3eae462e743e39a6c66cd9
Sean Chen <Sean.Chen@leocorn.com>
2020-03-11 14:57:36 -0400

    here is the complete test.

--- test/core/array-every-test.js
+++ test/core/array-every-test.js
@@ -1,9 +1,11 @@
 /**
  * basic usage for array.
  * - every function
- *
+ * The every() method tests whether all elements in the array pass the 
+ * test implemented by the provided function. It returns a Boolean value
  */
 
+// 2 demontion 
 let source = [
     ["a","b","c"],
     [1, 2, 3]
@@ -12,7 +14,12 @@ let source = [
 // show the source.
 console.table(source);
 
-source.every( (row, index) => {
+let result = source.every( (row, index) => {
     console.log(index);
     console.log(row);
+
+    // need return to check the next row!
+    return Array.isArray(row);
 } );
+
+console.log(result);
