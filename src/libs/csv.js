7819af6b9b2b05ede151513ecf4220138f60f52a
Sean Chen <Sean.Chen@leocorn.com>
2020-03-21 12:48:21 -0400

    get ready the strategy.

--- src/libs/csv.js
+++ src/libs/csv.js
@@ -18,12 +18,51 @@ let csv = {
     /**
      * process the given folder.
      */
-    processFolder: function(folder, localConfig) {
+    processFolder: function(theFolder, localConfig) {
+
+        let self = this;
+
+        let startTime = new Date();
+        console.log("Start to process all files for folder:", theFolder);
 
         // read all files:
-        let csvFiles = fs.readdirSync(folder);
+        let files = fs.readdirSync(theFolder);
         // quick check.
-        console.log(csvFiles);
+        //console.log(files);
+        // filter to only .csv files.
+
+        // set the waterfall iterator to process each file one after another.
+        // the function signature is defined in stragegy lib.
+        let waterfallIterator = function(index, reportOneFile) {
+
+            let oneFile = files[index];
+            if( oneFile.endsWith(".csv") ) {
+                self.processOneFile(theFolder + "/" + oneFile, reportOneFile,
+                                    localConfig);
+            } else {
+                console.log("---- Skip file:", oneFile);
+                reportOneFile(1);
+            }
+        };
+
+        // waterfall iterate through all files.
+        strategy.waterfallOver(0, files.length, waterfallIterator,
+            /**
+             * the callback function when the iteration is complete!
+             */
+            function() {
+
+                let totalTime = (new Date()) - startTime;
+                console.log("Complete processing all files!");
+                console.log("====Running time:", prettyMs(totalTime));
+            }
+        );
+    },
+
+    processOneFile: function(theFile, reportOneFileDone, localConfig) {
+
+        console.log("---- Process file:", theFile);
+        reportOneFileDone(1);
     }
 };
 
