efd2b754cfe75162ce409e0d35ff8ce2df087f4b
Sean Chen <Sean.Chen@leocorn.com>
2020-03-23 23:12:08 -0400

    add more testing.

--- test/data/csv-parse-test.js
+++ test/data/csv-parse-test.js
@@ -9,16 +9,17 @@ const parseCsv = require('csv-parse');
 const config = require('./../../src/config');
 const localConfig = config.loadCsvFolder;
 
-let fileName = "/usr/leocorn/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/01-22-2020.csv";
+//let fileName = "/usr/leocorn/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/01-22-2020.csv";
+let fileName = "/usr/leocorn/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/03-23-2020.csv";
 //let fileName = '/usr/leocorn/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/03-13-2020.csv';
 let fileContent = fs.readFileSync(fileName);
 //console.log(fileContent);
 
 parseCsv( fileContent, {columns: true},
     function(err, output) {
-        console.log(util.inspect(output[0]));
         //console.dir(output);
-        //console.table(output);
+        console.table(output);
+        console.log(util.inspect(output[0]));
 
         console.log("hasOwnProperty: ", output[0].hasOwnProperty('Province/State'));
         console.log('hasOwnProperty.call: ',
@@ -30,7 +31,8 @@ parseCsv( fileContent, {columns: true},
         //console.dir(Object.keys(output[0]));
         Object.keys(output[0]).forEach( key => {
             console.log(key, "=", output[0][key]);
-            console.log("===: ", key === 'Province/State');
+            console.log("Province/State ===: ", key === 'Province/State');
+            console.log("Province_State ===: ", key === 'Province_State');
         });
 
         //let payload = localConfig.tweakDocs(output, fileName);
