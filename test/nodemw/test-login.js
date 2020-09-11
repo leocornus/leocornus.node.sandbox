4fed1ebac4b56d3adbe3156e03f43603c75db84f
Sean Chen <Sean.Chen@leocorn.com>
2020-09-11 12:12:30 -0400

    quick login test.

---
+++ test/nodemw/test-login.js
@@ -0,0 +1,61 @@
+/*  
+    edit.js
+ 
+    MediaWiki API Demos
+    Demo of `Login` module: Sending post request to login
+    MIT license
+*/
+
+const request = require('request').defaults({jar: true});
+
+// we will execute the script by using nvm, for example:
+// $ nvm run node test-login.js
+const rawParams = process.argv.slice(2);
+console.log(rawParams);
+
+const url = "https://" + rawParams[0] + "/w/api.php";
+console.log(url);
+
+// Step 1: GET Request to fetch login token
+function getLoginToken() {
+    var params_0 = {
+        action: "query",
+        meta: "tokens",
+        type: "login",
+        format: "json"
+    };
+
+    request.get({ url: url, qs: params_0 }, function (error, res, body) {
+        if (error) {
+            return;
+        }
+        var data = JSON.parse(body);
+        loginRequest(data.query.tokens.logintoken);
+    });
+}
+
+// Step 2: POST Request to log in. 
+// Use of main account for login is not
+// supported. Obtain credentials via Special:BotPasswords
+// (https://www.mediawiki.org/wiki/Special:BotPasswords) for lgname & lgpassword
+function loginRequest(login_token) {
+    var params_1 = {
+        action: "login",
+        // we need use bot user account here.
+        lgname: rawParams[1],
+        lgpassword: rawParams[2],
+        lgtoken: login_token,
+        format: "json"
+    };
+
+    request.post({ url: url, form: params_1 }, function (error, res, body) {
+        if (error) {
+            return;
+        }
+        console.log(body);
+    });
+}
+
+// Start From Step 1
+getLoginToken();
+
