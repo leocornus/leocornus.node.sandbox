f38f1b84b9fb213d90a8619ec902e97ecb798191
Sean Chen <Sean.Chen@leocorn.com>
2020-09-14 15:27:53 -0400

    add error handling code

--- test/nodemw/pagesInCategory.js
+++ test/nodemw/pagesInCategory.js
@@ -2,12 +2,21 @@
 
 const Bot = require('nodemw');
 const client = new Bot({
-    server: 'en.wikipedia.org',
+    //server: 'en.wikipedia.org',
+    protocol: 'https',
+    server: 'demo.sites.leocorn.com',
     path: '/w',
-    debug: false
+    username: 'demoadmin',
+    password: 'oassmorq',
+    debug: true 
 });
 
-client.getPagesInCategory( 'Sports_cars', (err, pages) => {
+client.getPagesInCategory( 'Demo_on_Monday', (err, pages) => {
+
+    if(err) {
+        console.log(err);
+        return;
+    }
 
     console.log( 'Pages in category: %d', pages.length );
     //client.logData( pages );
