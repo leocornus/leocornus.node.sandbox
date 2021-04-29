4d7579dbea4928d3222eadbd9c772204c07d21cf
Sean Chen <Sean.Chen@leocorn.com>
2021-04-29 09:43:21 -0400

    some basic testing for luxon module

---
+++ test/luxon/basic.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const { DateTime } = require( 'luxon' );
+
+let today = DateTime.now();
+let zone = '';
+showToday( today, zone);
+
+// set time zone, using the identifier defined in this page:
+// - https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+zone = 'Asia/Shanghai';
+today = DateTime.now().setZone( zone );
+showToday( today, zone );
+
+zone = 'America/Toronto';
+today = DateTime.now().setZone( zone );
+showToday( today, zone );
+
+function showToday( today, zone ) {
+
+    console.table( {
+        "Time Zone": zone,
+        "ISO String": today.toISO(),
+        Year: today.year,
+        Month: today.month,
+        Day: today.day,
+    } );
+}
